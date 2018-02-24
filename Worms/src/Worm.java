7659f57c4710a535835c882e22d201a07a482b4e
SicariusLupus <32960489+SicariusLupus@users.noreply.github.com>
2018-02-24 17:58:53 +0100

    properties of the worm 1.1

--- Worms/src/Worm.java
+++ Worms/src/Worm.java
@@ -1,7 +1,16 @@
+import be.kuleuven.cs.som.annotate.*;
+
 /**
  * A class of worms with specific name, location, orientation, radius, 
  * mass and action points
- *  
+ * 
+ * @invar ...
+ * 		| isValidOrientation(getOrientation())
+ *  @invar ...
+ * 		| isValidRadius(getRadius())
+ *  @invar ...
+ * 		| isValidName(getName())
+ * 
  * @author Wolf Nuyts and Arnout Verboven
  *
  */
@@ -11,88 +20,202 @@ public class Worm {
 		// eerst massa voor getmaximumactionpoints
 		setActionPoints(getMaximumActionPoints());
 	}
-	private double x;
-	private double y;
+	
 	/**
-	 * 
+	 * Variable registering the x-coordinate of the worm in meters
 	 */
+	private double x;
+	
+	/**
+	 * Variable registering the y-coordinate of the worm in meters
+	 */
+	private double y;
+	
+	/**
+	 * returns the x-coordinate of the worm
+	 */
+	@Basic
 	public double getX() {
 		return x;
 	}
 	/**
-	 * 
+	 * returns the y-coordinate of the worm
 	 */
+	@Basic
 	public double getY() {
 		return y;
 	}
 	/**
-	 * 
-	 * @return
+	 * returns an array with the x- and y-coordinate of the worm
+	 * @return ...
+	 * 		| result == {getX(),getY()}
 	 */
 	public double[] getLocation() {
 		double[] loc = {getX(),getY()};
 		return loc;
 	}
 	/**
-	 * 
+	 * sets the x-coordinate of the worm
 	 * @param x
+	 * 		the new x-coordinate of the worm
+	 * @post ...
+	 * 		| new.getX() == x
 	 */
 	public void setX(double x) {
 		this.x = x;
 	}
+	
 	/**
-	 * 
+	 * sets the y-coordinate of the worm
 	 * @param y
+	 * 		the new y-coordinate of the worm
+	 *  @post ...
+	 * 		| new.getY() == y
 	 */
 	public void setY(double y) {
 		this.y = y;
 	}
 	
-	private double orientation;
 	/**
-	 * 
-	 * @return
+	 * Variable registering the orientation of the worm as an angle in radians
 	 */
+	private double orientation;
+	
+	/**
+	 * returns the orientation of the worm
+	 */
+	@Basic
 	public double getOrientation() {
 		return orientation;
 	}
 	/**
-	 * 
+	 * sets the orientation of the worm
 	 * @param orientation
+	 * 		the new orientation of the worm
+	 * @pre ...
+	 * 		|isValidOrientation(orientation)
+	 * @post ...
+	 * 		|new.orientation() == orientation
 	 */
 	public void setOrientation(double orientation) {
 		this.orientation = orientation;
 	}
 	
-	private double radius;
-	private final double minRadius = 0.25;
 	/**
-	 * 
-	 * @return
+	 * checks if orientation is valid
+	 * @param orientation
+	 * 		the orientation of the worm
+	 * @return ...
+	 * 		| result == (0 <= orientation < 2*Math.PI)
 	 */
+	public static boolean isValidOrientation(double orientation) {
+		return (0 <= orientation) && (orientation < 2*Math.PI);
+	}
+	
+	/**
+	 * Variabele registering the radius of the worm in meters
+	 */
+	private double radius;
+	
+	/**
+	 * Variable registering the minimum radius of the worm in meters
+	 */
+	private final double minRadius = 0.25; //0.25 kan later veranderen
+	
+	/**
+	 * returns the minimum radius of the worm
+	 */
+	@Basic
+	@Immutable
+	public double getMinRadius() {
+		return minRadius;
+	}
+	
+	/**
+	 * returns the radius of the worm
+	 */
+	@Basic
 	public double getRadius() {
 		return radius;
 	}
+	
 	/**
-	 * 
+	 * sets the radius of the worm
 	 * @param radius
+	 * 		the new radius of the worm
+	 * @post ...
+	 * 		| new.getRadius() = radius
+	 * @throws IllegalRadiusExcpetion
+	 * 			...
+	 * 		| ! isValidRadius(radius)
 	 */
-	public void setRadius(double radius) {
+	public void setRadius(double radius) throws IllegalRadiusException{ //exception nog maken
+		if (! isValidRadius(radius))
+			throw new IllegalRadiusException(radius);
 		this.radius = radius;
 	}
+	
+	/**
+	 *  checks if radius is valid
+	 * @param radius
+	 * 		the radius of the worm
+	 * @return ...
+	 * 		| radius >= getMinRadius()
+	 */
+	public boolean isValidRadius(double radius) {
+		return (radius >= getMinRadius());
+	}
+	
+	/**
+	 * Variable registering the density of the worm in Kg per m³
+	 */
 	private final double density = 1062;
+	
+	/**
+	 * returns the mass of the worm in kg
+	 * @return ...
+	 * 		| result == density * 4/3*Math.PI * Math.pow(radius, 3.0)
+	 */
 	public double getMass() {
 		return (density * 4/3*Math.PI * Math.pow(radius, 3.0));
 	}
 	
+	/**
+	 * returns the maximum amount of action points
+	 * @return ...
+	 * 		| result == Math.round(getMass())
+	 */
 	public int getMaximumActionPoints() {
 		return (int) Math.round(getMass());
 	}
 	
+	/**
+	 * Variable registering the amount of action points of the worm
+	 */
 	private int actionPoints;
+	
+	/**
+	 *returns the amoiunt of action points of the worm
+	 */
+	@Basic
 	public int getActionPoints() {
 		return actionPoints;
 	}
+	
+	/**
+	 * sets the amount of action points of the worm
+	 * @param actionPoints
+	 * 		the new amount of action points of the worm
+	 * @post ...
+	 * 		| if ((actionPoints >= 0) && (actionPoints <= getMaximumActionPoints()))
+	 * 		  new.getActionPoints() == actionPoints
+	 * @post ...
+	 * 		| if (actionPoints < 0)
+			  new.getActionPoints() == 0
+	 * @post ...
+	 * 		| if (actionPoints > getMaximumActionPoints())
+	 * 		  new.getActionPoints() == getMaximumActionPoints();
+	 */
 	public void setActionPoints(int actionPoints) {
 		if ((actionPoints >= 0) && (actionPoints <= getMaximumActionPoints()))
 			this.actionPoints = actionPoints;
@@ -102,14 +225,64 @@ public class Worm {
 			this.actionPoints = getMaximumActionPoints();		
 	}
 	
+	/**
+	 * Variable registering the name of the worm
+	 */
 	private String name;
+	
+	/**
+	 *returns the name of the worm
+	 */
+	@Basic
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	
+	/**
+	 * sets the name of the worm
+	 * @param name
+	 * 		the new name of the worm
+	 * @post ...
+	 * 		| new.getName() == name
+	 * @throws IllegalNameException
+	 * 		| ! isValidName(name)
+	 */
+	public void setName(String name) throws IllegalNameException {
+		if (! isValidName(name))
+			throw new IllegalNameException(name);
 		this.name = name;
 	}
 	
+	/**
+	 * checks if the name is valid
+	 * @param name
+	 * 		the name of the worm
+	 * @return ...
+	 * 		| result == ((name.length() >= 2) && (Character.isUpperCase(name.charAt(0)))
+				&& (validCharacters))
+	 */
+	public static boolean isValidName(String name) {
+		boolean validCharacters = true;
+		for (int i = 0; i<name.length(); i++) {
+			if (!isValidCharacter(name.charAt(i)))
+				validCharacters = false;
+		}
+						
+		return ((name.length() >= 2) && (Character.isUpperCase(name.charAt(0)))
+				&& (validCharacters));
+		
+	}
+	
+	/**
+	 * checks if the character is valid
+	 * @param c
+	 * 		the checked character
+	 * @return ...
+	 * 		| result == ((Character.isLetter(c)) || (c == ' ') || (c == '\'') || (c == '"'))
+	 */
+	public static boolean isValidCharacter(char c) {
+		return ((Character.isLetter(c)) || (c == ' ') || (c == '\'') || (c == '"'));
+	}
 }
 
 
